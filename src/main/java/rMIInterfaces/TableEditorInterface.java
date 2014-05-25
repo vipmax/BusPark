46c03e46efa45bc03d8e892c24dfc4cbed9c4ab9
thergbway <thergbworld@gmail.com>
2014-05-26 02:27:38 +0400

    Rearranged folders. Added new project structure.

---
+++ src/main/java/rMIInterfaces/TableEditorInterface.java
@@ -0,0 +1,4 @@
+package rMIInterfaces;
+
+public interface TableEditorInterface {
+}
---
+++ src/main/java/tableEditor/Main.java
@@ -0,0 +1,7 @@
+package tableEditor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("table editor has been started");
+    }
+}
