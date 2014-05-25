50827d29f39a55fa49d248430d6639a0a836a21c
vipmax <djvipmax@gmail.com>
2014-05-25 22:27:25 +0400

    add first method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(1);
     }
+
+    public static void sayHello() {
+        System.out.println("hello");
+    }
+
 }
